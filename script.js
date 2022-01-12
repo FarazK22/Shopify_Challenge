1f97348a48725343d2ceb81da00750cff3eef199
Faraz Kazi <faraz.kazi22@gmail.com>
2022-01-11 22:36:07 -0800

    Fetched on load, added basic bootstrap styling and card, next step is to implement multiple card loading

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-const likeButton = document.querySelector('#like')
-const unlikeButton = document.querySelector('#unlike')
-const spaceHead = document.querySelector('#spaceHead')
-const image = document.querySelector('#container');
+const likeButton = document.querySelector('#like');
+const unlikeButton = document.querySelector('#unlike');
+const fetchButton = document.querySelector('#fetch');
+const spaceHead = document.querySelector('#spaceHead');
+const image = document.querySelector('#nasaImage');
 
 
 
 // Listen's for user's click on 'Spacestagram' header
-spaceHead.addEventListener('click', () => {
-    sendApiRequest();
-})
+// fetchButton.addEventListener('click', () => {
+//     alert('clicked!')
+//     sendApiRequest();
+// })
 
-// Asynchronous function that sends the API request to NASA APOD API
-async function sendApiRequest() {
-    const API_Key = 'mdeebkyai5eBWBqfW7B06JfCMhUqyN3ZiNRcMEl5'
-    let res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_Key}`)
-    let data = await res.json();
-    console.log(data);
-    pullApiImage(data);
-}
-
-// Converts API data into an image that is displayed on screen
-function pullApiImage(data) {
-    document.querySelector('#container').innerHTML = `<img id='nasa' src= "${data.url}">`
-    const nasaImg = document.querySelector('#nasa');
-    nasaImg.classList.add('item');
-
-}
 
 
 
